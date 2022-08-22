14d08df4cf3c31575b036cf5243e16eb0104e7cc
AsiburSumon <sumonasif68@gmail.com>
2022-08-22 11:06:23 +0600

    total calculate

--- js/budget.js
+++ js/budget.js
@@ -1,6 +1,22 @@
 document.getElementById('btn-calculate').addEventListener('click',function(){
+    playerExpenses();
+})
+
+function playerExpenses(){
     const perPlayer = getElementValueById('per-player');
-    let forPlayerExpenses = getTextElementValueById('playerExpenses');
+    const forPlayerExpenses = getTextElementValueById('playerExpenses');
     setTextElementValue('playerExpenses', perPlayer*5);
+    return forPlayerExpenses;
+}
+
+document.getElementById('calculateTotal').addEventListener('click', function(){
+    const managerCost = getElementValueById('manager-cost');
+    const coachCost = getElementValueById('coach-cost');
+    const playerCost = playerExpenses();
+    
+    const totalCost = managerCost+coachCost+playerCost;
+
+    const total = getTextElementValueById('total');
+    setTextElementValue('total', totalCost);
 })
     
